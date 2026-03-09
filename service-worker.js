0d1af930c1c4cc12e4a1c8e236dede896960fe28
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-03-09 16:21:19 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = 'match-edition-v3.0';
+const ASSETS = [
+  './',
+  './index.html',
+  '/competition-Scorer/manifest.json',
+  '/competition-Scorer/icon.png'
+];
 
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
